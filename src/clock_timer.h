9e4ff2400f6df9bd7c09f69014941cc2751b56aa
stash-s <staszek.salik@gmail.com>
2018-05-11 21:57:02 +0200

    Removed compiler warning

--- src/clock_timer.h
+++ src/clock_timer.h
@@ -1,22 +1,23 @@
 #ifndef __CLOCK_TIMER_H__
 #define __CLOCK_TIMER_H__
 
-#include "osapi.h"
+#include <osapi.h>
 #include <time.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef struct {
+typedef struct  _clock_timer my_clock_timer_t;
+
+struct _clock_timer {
     uint8_t   *digits;
     int        digits_max;
     struct tm  tm;
 
     int        dot_position;
-    os_timer_t timer
-
-} my_clock_timer_t;
+    os_timer_t timer;
+};
 
 extern my_clock_timer_t *my_clock_timer;
 void clock_timer_init (uint8_t *digits, int digits_max);
